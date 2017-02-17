604e83c284fdfc8c4287e21d327e4a377a09c64f
Felipe Soares <felipeluizsoares@gmail.com>
2017-02-17 14:51:06 -0200

    Use React context to manage state through routes

--- src/components/router/Link.js
+++ src/components/router/Link.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 
 class Link extends Component {
+  static contextTypes = {
+    route: React.PropTypes.string,
+    linkHandler: React.PropTypes.func
+  }
+
   handleClick = (evt) => {
     evt.preventDefault();
 
-    history.pushState(null, '', this.props.to);
+    this.context.linkHandler(this.props.to);
   }
 
   render () {
+    const activeClass = this.context.route === this.props.to ? 'active' : '';
+
     return (
-      <a href="#" onClick={this.handleClick}> {this.props.children} </a>
+      <a href="#" onClick={this.handleClick} className={activeClass}> {this.props.children} </a>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import Router from './components/router/Router';
 
 ReactDOM.render(
-  <App />,
+  <Router><App /></Router>,
   document.getElementById('root')
 );
